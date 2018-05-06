14c3b485cdd3538a8df53f1b02b45d647f7c4fc4
Andrea Massetti <andrea.massett@gmail.com>
2018-05-07 02:31:16 +1000

    add the class from which everything else will inherit

---
+++ world_functions.py
@@ -0,0 +1,16 @@
+"""
+IO_trasf 
+IO"""
+
+def __tra_L_photosynthesis():
+    pass
+def __tra_D_photosynthesis():
+    pass
+def __tra_growth():
+    pass
+def __tra_respiration():
+    pass
+def __IO_internal_draw():
+    pass
+def __IO_external_draw():
+    pass
