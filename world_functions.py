ec04c48c6a992de8ff504142cfa1d8cac9e4f172
Andrea Massetti <andrea.massett@gmail.com>
2018-05-07 12:58:54 +1000

    copied photosyntesis, still much to do!

--- world_functions.py
+++ world_functions.py
@@ -3,7 +3,15 @@ IO_trasf
 IO"""
 
 def __tra_L_photosynthesis():
-    pass
+    """tests availability of resources and produces new sugar consuming other resources"""
+        
+    if self.inflow_test("H2O",6) and self.stomatain_test("CO2",6) and self.air.solar==True:#light phase
+        self.inflow_do("H2O",6)#draws water            
+        self.stomatain_do("CO2",6)#draws CO2   
+        self.logword.append("the sugar changes due to photosynthesis from {} to {} due to a 6 x {}(growthstage)= {}\n".format(round(self.sugar,2),round(self.sugar+self.growth_stage*6,2),round(self.growth_stage,2),round(self.growth_stage*6,2)))
+        self.sugar+=self.growth_stage*6
+            
+        self.stomataout("O2",6)#returns O2
 def __tra_D_photosynthesis():
     pass
 def __tra_growth():
